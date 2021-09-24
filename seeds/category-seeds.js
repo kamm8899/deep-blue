bf91d6ddd784fb5fb4e41b59b2f087a50441f9f5
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-09-23 21:05:12 -0400

    added categories to seeds

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -8,6 +8,24 @@ const categoryData = [
 */
 
 //Start seeds here
+{
+    category_name: 'Cave Explorers'
+},
+{
+    category_name: 'Pearl Divers'
+},
+{
+    category_name: 'Nether Jewels'
+},
+{
+    category_name: 'Anchor Bottoms'
+},
+{
+    category_name: 'Pearl Divers'
+},
+{
+    category_name: 'falsdj;fasjdfl;kjasdl;fkj'
+},
 
 ]
 
