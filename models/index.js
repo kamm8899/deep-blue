6c5c2979b8af5756031587744f5bc3f21e281566
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-26 14:40:18 -0400

    added auth module to utils folder

--- models/index.js
+++ models/index.js
@@ -13,10 +13,10 @@ Product.belongsTo(Category, {
 });
 
 //added User associations, not directly associated with each other, cart product_id and user_id
-User.belongsToMany(Product,{
-   through: Cart, as:'cart', foreignKey: 'product_id', onDelete: 'cascade'});
-Product.belongsToMany(User,{
-    through: Cart, as:'cart', foreignKey: 'user_id', onDelete: 'cascade'});
+// User.belongsToMany(Product,{
+//    through: Cart, as:'cart', foreignKey: 'product_id', onDelete: 'cascade'});
+// Product.belongsToMany(User,{
+//     through: Cart, as:'cart', foreignKey: 'user_id', onDelete: 'cascade'});
 
 
 module.exports = {
