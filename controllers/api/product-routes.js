a5e888c5fc7fd634f0433fbb3b033b7ef8b7dc5a
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-26 14:59:37 -0400

    fixed but in schema.sql

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -2,66 +2,86 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth.js');
 
-const { Product,Category, User} = require('../../models');
+const { Product, Category, User } = require('../../models');
 
 //get all Users
 router.get('/', (req, res) => {
-    Product.findAll({
-        attributes: [
-            'id',
-            'product_name',
-            'price',
-            'stock', 
-        ],
-        include: [
-            {
-            model: Category,
-            attributes:['category_id', 'category_name'], 
-        },
-        {
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_id', 'category_name'],
+      },
+      {
 
-            model: User,
-            attributes: ['email']
-          }
-        ]
-      })
-
-    })
+        model: User,
+        attributes: ['email']
+      }
+    ]
+  })
     .then(dbProductData => {
-        if (!dbProductData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbProductData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-//Find one Product
-router.get('/:id', (req, res) =>{
-    Product.findOne({
-        where:{
-            id: req.params.id,
-        }, 
-        attributes: [
-            'id',
-            'product_name',
-            'price',
-            'stock', 
-        ],
-        include: [
-            {
-            model: Category,
-            attributes:['category_id', 'category_name'], 
-        },
-        {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
 
-            model: User,
-            attributes: ['email']
-          }
-        ]
-      })
+//Find one Product
+router.get('/:id', (req, res) => {
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_id', 'category_name'],
+      },
+      {
+
+        model: User,
+        attributes: ['email']
+      }
+    ]
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+//create new Product
+router.post('/', withAuth, (req, res) => {
+  if (req.session) {
+    Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      category_id: req.session.category_id,
+    })
       .then(dbProductData => {
         if (!dbProductData) {
           res.status(404).json({ message: 'No post found with this id' });
@@ -73,77 +93,56 @@ router.get('/:id', (req, res) =>{
         console.log(err);
         res.status(500).json(err);
       });
-    });
-    //create new Product
-    router.post('/', withAuth, (req, res) => {
-        if (req.session) {
-          Product.create({
-            product_name: req.body.product_name,
-            price: req.body.price,
-            category_id: req.session.category_id,
-          })
-          .then(dbProductData => {
-            if (!dbProductData) {
-              res.status(404).json({ message: 'No post found with this id' });
-              return;
-            }
-            res.json(dbProductData);
-          })
-          .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-    });
-}
+  }
 });
 
 //update product name
 router.put('/:id', (req, res) => {
-    Product.update(
-      {
-        product_name: req.body.product_name
-      },
-      {
-        where: {
-          id: req.params.id
-        }
-      }
-    )
-      .then(dbProductData => {
-        if (!dbProductData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbProductData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-  //route for delete product
-  router.delete('/:id', withAuth, (req, res) => {
-    console.log('id', req.params.id);
-    Product.destroy({
+  Product.update(
+    {
+      product_name: req.body.product_name
+    },
+    {
       where: {
         id: req.params.id
       }
+    }
+  )
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbProductData);
     })
-      .then(dbProductData => {
-        if (!dbProductData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbProductData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-    
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//route for delete product
+router.delete('/:id', withAuth, (req, res) => {
+  console.log('id', req.params.id);
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+});
 
 
-    module.exports= router;
+module.exports = router;
 
-   
