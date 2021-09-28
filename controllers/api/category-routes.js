c8d891c75b88709e8a0ea5ac6fd1598a664fc8e4
kamm8899 <kamm8899@gmail.com>
2021-09-28 06:49:15 -0700

    Revert "updated Category routes" removing commit
    This reverts commit 2aa2df5acf9ccff9c4989cee550479f6f1a814a5.

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/category` endpoint
+// The `/api/categy` endpoint
 router.get('/', (req, res) => {
     // find all category
     // be sure to include its associated Products
