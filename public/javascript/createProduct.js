2d37c10f89d50f1655ecba6074d596f8630ff10f
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-10-01 12:45:54 -0400

    added working category dropdown for new products

--- public/javascript/createProduct.js
+++ public/javascript/createProduct.js
@@ -4,24 +4,25 @@ const price = $('#price');
 const stock = $('#stock');
 const categoryId = $('#categoryId');
 
-
-
-//get categories for the selection form
-// const getCategories = () => {
-//     axios.get("/api/category")
-//     .then(response => {
-//         const cat = response.data;
-//         console.log(cat);
-//         for(let i = 0; i<cat.length; i++){
-//             const categoryId = cat[i].id;
-//             const categoryName = cat[i].category_name;
-
-//             let option = $("<option>").addClass(categoryId).text(categoryName);
-//             $(".form-select").append(option);
-//         }
-//     })
-//     .catch(error => console.log(error));
-// }
+$(document).ready(function() {
+    //get categories for the selection form
+    const getCategories = () => {
+        axios.get("/api/category")
+        .then(response => {
+            const cat = response.data;
+            console.log(cat);
+            for(let i = 0; i<cat.length; i++){
+                const categoryId = cat[i].id;
+                const categoryName = cat[i].category_name;
+                let option = $("<option>").text(categoryName).val(categoryId);
+                $(".form-select").append(option);
+            }
+        })
+        .catch(error => console.log(error));
+    }
+   
+    getCategories();
+});  
 
 // funciton that will save new product to database
 const addNewProduct = () => {
